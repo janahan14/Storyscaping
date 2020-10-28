d32152fbcf21eb29a197fb394eaa0259a0c780ab
Mano <mano-1412@hotmail.com>
2020-10-28 13:16:32 +0100

    pluginpt13

--- wp-content/plugins/partyvibesplugin/index.php
+++ wp-content/plugins/partyvibesplugin/index.php
@@ -17,7 +17,7 @@ function newsletter_form()
     $content .= '<div class="popupCloseButton">X</div>';
   	$content .= '<section>';
     $content .= '<h3 id="velkommen">Vi har plads til dig, når vi åbner huset</h3>';
-    $content .= '<video playsinline autoplay muted type="video/mp4" id="logo" src=" '.plugins_url("partyvibesplugin/video/calender.mp4").' " ';
+    $content .= '<video playsinline autoplay muted loop type="video/mp4" id="logo" src=" '.plugins_url("partyvibesplugin/video/calender.mp4").' " ';
     $content .= 'alt="thlvideo">';
   	$content .= '<h5 id="tilmeld">Tilmeld dig her, og bliv opdateret, når vi holder åbent hus</h5>';
   	$content .= '</section>';
